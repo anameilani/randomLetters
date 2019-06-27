c5f5309d1bae2f6f39780cc3fbe8e1b822ad10c4
anameilani <meilani.ana@gmail.com>
2019-06-27 19:44:13 +0700

    intial game

--- client/src/router.js
+++ client/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import GamePage from './views/Game.vue'
 
 Vue.use(Router)
 
@@ -10,8 +11,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'gamePage',
+      component: GamePage
     },
     {
       path: '/about',
